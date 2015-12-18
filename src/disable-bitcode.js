27a623b24dc7c9e8d70b27c2c45a4ff07a71919e
mikepowroznik <mike@one45.com>
2015-12-18 15:15:22 -0800

    adding arch settings

--- src/disable-bitcode.js
+++ src/disable-bitcode.js
@@ -13,13 +13,14 @@ var projectName = new ConfigParser(util.projectConfig(util.isCordova())).name();
 var projectPath = './platforms/ios/' + projectName + '.xcodeproj/project.pbxproj';
 var myProj = xcode.project(projectPath);
 
-myProj.parse(function (err) {
-  if(err){
+myProj.parse(function(err) {
+  if (err) {
     console.log('Error: ' + JSON.stringify(err));
-  }
-  else{
+  } else {
     myProj.updateBuildProperty('ENABLE_BITCODE', 'NO');
+    myProj.updateBuildProperty('VALID_ARCHS', '"armv7 arm64"');
+    myProj.updateBuildProperty('ARCHS', '"armv7 arm64"');
     fs.writeFileSync(projectPath, myProj.writeSync());
-    console.log('✔ BITCODE disable');
+    console.log('BITCODE disable');
   }
 });
